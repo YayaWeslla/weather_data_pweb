79e28b0c72426cae5f0d422858c9ccb2295773bb
YayaWeslla <yws2@aluno.ifal.edu.br>
2025-08-13 20:59:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,74 @@
-//Função para buscar o nome da cidade
-async function getCityName() {
-    const cidadeInput = document.getElementById('city-input');
-    return cidadeInput.value.trim();
-}
+//Variáveis
+const apiKey = 'd494fc4ee0474aa587c235632251108';
+const botaoPesquisa = document.getElementById('search-btn');
+const inputCidade = document.getElementById('city-input');
+const cardClima = document.getElementById('weather-result');
+const erro = document.getElementById('error-message');
 
-//Função para buscar os dados do clima
-async function getWeatherData(nomeCidade) {
+async function getClima(cidade) {
     try{
-        const keyApi = 'd494fc4ee0474aa587c235632251108';
-        const apiCaminho = `https://api.weatherapi.com/v1/current.json?key=${keyApi}&q=${nomeCidade}&aqi=no&lang=pt`;
-
-        const response = await fetch(apiCaminho);
-        if (!response.ok) {
-            throw new Error(`Cidade ${nomeCidade}não encontrada! Verifique o nome e tente novamente.`);
+        const nomeCidade = cidade.toLowerCase().trim();
+        const ulr = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${nomeCidade}&aqi=no&lang=pt`);
+        if (!ulr.ok){
+            throw new Error(`A cidade ${cidade} não foi encontrada! Verifique o nome da cidade e tente novamente.`);
         }
-        const data = await response.json();
-        console.log(data);
-        displayWeatherData(data);
+        const dados = await ulr.json();
+        dadosClima(dados);
+        erro.classList.add('hidden');
+    } catch (e){
+        erro.textContent = e.message;
+        erro.classList.remove('hidden');
+        cardClima.classList.add('hidden');
     }
-    
-
-        
-        .then(data => {
-            console.log(data);
-            displayWeatherData(data);
-        })
-        .catch(error => {
-            console.error(error);
-            showError();
-        });
 }
+
+function dadosClima(dadosCidade){
+    cardDadosClima = `
+    <h2 id="city-name">${dadosCidade.location.name}, ${dadosCidade.location.country}</h2>
+    <p id "local-time" class="local-time>Horário local: ${dadosCidade.location.localtime}</p>
+
+    <div class="weather-main">
+        <img id="weather-icon" src="${dadosCidade.current.condition.icon}" alt="Ícone do clima">
+        <p id="temperature">${dadosCidade.current.temp_c}°C</p>
+    </div>
+    <p id="condition">${dadosCidade.current.condition.text}</p>
+
+    <div class ="weather-details">
+        <div = class="detail-item">
+            <span>Sensação</span>
+            <strong id="feels-like">${dadosCidade.current.feelslike_c}°</strong>
+        </div>
+        <div class="detail-item">
+            <span>Umidade</span>
+            <strong id="humidity">${dadosCidade.current.humidity}%</strong>
+        </div>
+        <div class="detail-item">
+            <span>Vento</span>
+            <strong id="wind-speed">${dadosCidade.current.wind_kph} km/h</strong>
+        </div>
+        <div class="detail-item">
+            <span>Pressão</span>
+            <strong id="pressure">${dadosCidade.current.pressure_mb} mb</strong>
+        </div>
+        <div class="detail-item">
+            <span>Visibilidade</span>
+            <strong id="visibility">${dadosCidade.current.vis_km} km</strong>
+        </div>
+        <div class="detail-item">
+            <span>Índice UV</span>
+            <strong id="uv-index">${dadosCidade.current.uv}</strong>
+        </div>
+    </div>
+    `
+    cardClima.innerHTML = cardDadosClima;
+    cardClima.classList.remove('hidden');
+}
+
+
+botaoPesquisa.addEventListener('click', function(){
+    const nomeCidade = inputCidade.value.trim();
+
+    if(nomeCidade){
+        getClima(nomeCidade);
+    }
+})
